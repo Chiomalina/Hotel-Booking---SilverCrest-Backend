89b993671a2c429bde29fb64e51d388c5e76123a
iamchioma <chiomalinaanaso@gmail.com>
2026-03-18 03:57:44 +0100

    feat(room-route): creat API functionality to getRooms, getOwnerRooms, createRoom toggleRoomAvailability and register the controller in the room routes and server

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,12 +2,12 @@ import User from "../models/User.js";
 
 // Middleware to chack if the user is authenticated
 export const protect = async (req, res, next) => {
-    const {userId} = req.auth;
-    if (!userId) {
-        res.json({sucess: false, messag: "not authenticated"})
-    } else {
-        const user = await User.findById(userId);
-        req.user = user;
-        next()
-    }
-}
+  const { userId } = req.auth;
+  if (!userId) {
+    res.json({ success: false, message: "not authenticated" });
+  } else {
+    const user = await User.findById(userId);
+    req.user = user;
+    next();
+  }
+};
